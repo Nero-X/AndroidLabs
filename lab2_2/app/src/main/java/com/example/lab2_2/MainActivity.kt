b1b3bc90734a03dc1d14e11c42b52ceecfad7480
Nero-X <SashaJCV@yandex.ru>
2020-10-03 17:58:54 +0300

    finished labs 3 and 4

--- lab2_2/app/src/main/java/com/example/lab2_2/MainActivity.kt
+++ lab2_2/app/src/main/java/com/example/lab2_2/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.lab2_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.Toast
@@ -13,28 +15,44 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         button_add.setOnClickListener {
-            textView_res.text = (num1.text.toString().toInt() + num2.text.toString().toInt()).toString()
+            textView_res.text =
+                (num1.text.toString().toInt() + num2.text.toString().toInt()).toString()
         }
         button_sub.setOnClickListener {
-            textView_res.text = (num1.text.toString().toInt() - num2.text.toString().toInt()).toString()
+            textView_res.text =
+                (num1.text.toString().toInt() - num2.text.toString().toInt()).toString()
         }
         button_mul.setOnClickListener {
-            textView_res.text = (num1.text.toString().toInt() * num2.text.toString().toInt()).toString()
+            textView_res.text =
+                (num1.text.toString().toInt() * num2.text.toString().toInt()).toString()
         }
         button_div.setOnClickListener {
-            try {
-                textView_res.text = (num1.text.toString().toDouble() / num2.text.toString().toInt()).toString()
-            }
-            catch (e: ArithmeticException)
+            textView_res.text = (num1.text.toString().toDouble() / num2.text.toString().toInt()).toString()
+            if (num2.text.toString().toInt() == 0)
             {
-                textView_res.text = "\u221e"
-                var toast = Toast.makeText(applicationContext, "Деление на ноль!", Toast.LENGTH_LONG)
-                /*var imgView = ImageView(applicationContext)
-                imgView.setImageResource(R.drawable.zero)*/
-                /*var toastContainer = toast.view as LinearLayout
-                toastContainer.addView(imgView)*/
+                var toast = Toast.makeText(this, "Деление на ноль!", Toast.LENGTH_LONG)
+                var imgView = ImageView(applicationContext)
+                    imgView.setImageResource(R.drawable.zero)
+                var toastContainer = toast.view as LinearLayout
+                    toastContainer.addView(imgView, 450, 450)
                 toast.show()
             }
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId)
+        {
+            R.id.add -> button_add.callOnClick()
+            R.id.sub -> button_sub.callOnClick()
+            R.id.mul -> button_mul.callOnClick()
+            R.id.div -> button_div.callOnClick()
+        }
+        return true
     }
 }
