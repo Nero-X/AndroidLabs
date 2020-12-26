7f6868e363aea265f9bc07286cf667c855349cdb
Nero-X <SashaJCV@yandex.ru>
2020-12-26 16:24:45 +0300

    fixed lab9 and added lab11

--- lab11/app/src/main/java/com/example/lab11/MainActivity.kt
+++ lab11/app/src/main/java/com/example/lab11/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.lab11
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        openMap.setOnClickListener {
+            val coordRegex = Regex("^(-?\\d+(\\.\\d+)?),\\s*(-?\\d+(\\.\\d+)?)\$")
+            var geoURI = ""
+            geoURI = if (coordRegex.matches(address.text)) {
+                "geo:${address.text}z=8"
+            } else {
+                "geo:0,0?q=${address.text}&z=8"
+            }
+            val geo = Uri.parse(geoURI)
+            val geoIntent = Intent(Intent.ACTION_VIEW, geo)
+            startActivity(geoIntent)
+        }
+
+        openStreet.setOnClickListener {
+            val coordRegex = Regex("^(-?\\d+(\\.\\d+)?),\\s*(-?\\d+(\\.\\d+)?)\$")
+            var geoURI = ""
+            geoURI = if (coordRegex.matches(address.text)) {
+                "google.streetview:cbll=${address.text}&cbp=1,99.56,,1,2.0&mz=19"
+            } else {
+                "geo:0,0?q=${address.text}&z=8"
+            }
+            val geo = Uri.parse(geoURI)
+            val geoIntent = Intent(Intent.ACTION_VIEW, geo)
+            startActivity(geoIntent)
+        }
     }
 }
